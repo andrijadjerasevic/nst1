74b937b5d8ba548a0965f82a6650b4c8f55b8b45
Andrija Djerasevic <andrija.djerasevic@gmail.com>
2022-04-22 20:21:44 +0200

    Database dump

--- resources/databaseDump/database.sql
+++ resources/databaseDump/database.sql
@@ -21,45 +21,53 @@ USE `nst1`;
 DROP TABLE IF EXISTS `admin`;
 
 CREATE TABLE `admin` (
-  `adminId` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) COLLATE utf16_bin DEFAULT NULL,
-  `password` varchar(100) COLLATE utf16_bin DEFAULT NULL,
-  PRIMARY KEY (`adminId`)
+  `adminEmail` varchar(100) COLLATE utf16_bin NOT NULL,
+  `adminPassword` varchar(100) COLLATE utf16_bin NOT NULL,
+  PRIMARY KEY (`adminEmail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 /*Data for the table `admin` */
+
+insert  into `admin`(`adminEmail`,`adminPassword`) values 
+('andrija.djerasevic@gmail.com','andrija'),
+('pera.peric@gmail.com','pera1');
 
 /*Table structure for table `employee` */
 
 DROP TABLE IF EXISTS `employee`;
 
 CREATE TABLE `employee` (
-  `employeeId` int(11) NOT NULL AUTO_INCREMENT,
+  `employeeEmail` varchar(100) COLLATE utf16_bin NOT NULL,
   `firstName` varchar(100) COLLATE utf16_bin DEFAULT NULL,
   `lastName` varchar(100) COLLATE utf16_bin DEFAULT NULL,
-  `email` varchar(100) COLLATE utf16_bin DEFAULT NULL,
-  `adminId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`employeeId`),
-  KEY `adminId` (`adminId`),
-  CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `admin` (`adminId`) ON DELETE CASCADE ON UPDATE CASCADE
+  `adminEmail` varchar(100) COLLATE utf16_bin NOT NULL,
+  PRIMARY KEY (`employeeEmail`),
+  KEY `adminEmail` (`adminEmail`),
+  CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`adminEmail`) REFERENCES `admin` (`adminEmail`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 /*Data for the table `employee` */
+
+insert  into `employee`(`employeeEmail`,`firstName`,`lastName`,`adminEmail`) values 
+('andrija.djerasevic@gmail.com','Andrija','Djerasevic','andrija.djerasevic@gmail.com');
 
 /*Table structure for table `participate` */
 
 DROP TABLE IF EXISTS `participate`;
 
 CREATE TABLE `participate` (
-  `employeeId` int(11) NOT NULL,
+  `employeeEmail` varchar(100) COLLATE utf16_bin NOT NULL,
   `projectId` int(11) NOT NULL,
-  PRIMARY KEY (`employeeId`,`projectId`),
-  KEY `participateFk2` (`projectId`),
-  CONSTRAINT `participateFk1` FOREIGN KEY (`employeeId`) REFERENCES `employee` (`employeeId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `participateFk2` FOREIGN KEY (`projectId`) REFERENCES `project` (`projectId`) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (`employeeEmail`,`projectId`),
+  KEY `projectId` (`projectId`),
+  CONSTRAINT `participate_ibfk_1` FOREIGN KEY (`employeeEmail`) REFERENCES `employee` (`employeeEmail`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `participate_ibfk_2` FOREIGN KEY (`projectId`) REFERENCES `project` (`projectId`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 /*Data for the table `participate` */
+
+insert  into `participate`(`employeeEmail`,`projectId`) values 
+('andrija.djerasevic@gmail.com',16);
 
 /*Table structure for table `project` */
 
@@ -68,31 +76,20 @@ DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
   `projectId` int(11) NOT NULL AUTO_INCREMENT,
   `projectName` varchar(100) COLLATE utf16_bin DEFAULT NULL,
+  `projectLocation` varchar(100) COLLATE utf16_bin DEFAULT NULL,
+  `projectDescription` varchar(1000) COLLATE utf16_bin DEFAULT NULL,
   `startDate` datetime DEFAULT NULL,
   `endDate` datetime DEFAULT NULL,
-  `adminId` int(11) DEFAULT NULL,
+  `adminEmail` varchar(100) COLLATE utf16_bin NOT NULL,
   PRIMARY KEY (`projectId`),
-  KEY `adminId` (`adminId`),
-  CONSTRAINT `project_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `admin` (`adminId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+  KEY `adminEmail` (`adminEmail`),
+  CONSTRAINT `project_ibfk_1` FOREIGN KEY (`adminEmail`) REFERENCES `admin` (`adminEmail`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 /*Data for the table `project` */
 
-/*Table structure for table `task` */
-
-DROP TABLE IF EXISTS `task`;
-
-CREATE TABLE `task` (
-  `taskId` int(11) NOT NULL AUTO_INCREMENT,
-  `taskName` varchar(100) COLLATE utf16_bin DEFAULT NULL,
-  `description` varchar(1000) COLLATE utf16_bin DEFAULT NULL,
-  `projectId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`taskId`),
-  KEY `taskFk1` (`projectId`),
-  CONSTRAINT `taskFk1` FOREIGN KEY (`projectId`) REFERENCES `project` (`projectId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
-
-/*Data for the table `task` */
+insert  into `project`(`projectId`,`projectName`,`projectLocation`,`projectDescription`,`startDate`,`endDate`,`adminEmail`) values 
+(16,'Test','Belgrade','Testing','2022-04-23 18:37:53','2022-04-24 18:37:57','andrija.djerasevic@gmail.com');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
